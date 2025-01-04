335d8482e5c701a86e67ec75e25e91c3584483cb
AmitBenEzraColman <amit922001@cs.colman.ac.il>
2025-01-05 00:18:15 +0200

    bodyParser

--- src/app.js
+++ src/app.js
@@ -3,17 +3,19 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
 const mongoose = require("mongoose");
-mongoose.connect(process.env.DB_CONNECTION, {
-  useNewUrlParser: true
-})
+mongoose.connect(process.env.DB_CONNECTION)
 const db = mongoose.connection;
 db.on("error", (error) => console.log("error", error))
-db.once("open", () => console.log("connect"))
+db.once("open", () => console.log("connected to DB"))
 
 
 const postRoutes = require("./routes/postsRoute")
-app.use("/post", postRoutes)
+app.use("/posts", postRoutes)
 
 app.get('/', (req, res) => {
   res.send('check');
