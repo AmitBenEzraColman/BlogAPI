657ca7872814e1fd0683b173737987e22e3cae05
AmitBenEzraColman <amit922001@cs.colman.ac.il>
2025-01-05 00:01:34 +0200

    add mongoose

--- src/app.js
+++ src/app.js
@@ -3,6 +3,15 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT
 
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DB_CONNECTION, {
+  useNewUrlParser: true
+})
+const db = mongoose.connection;
+db.on("error", (error) => console.log("error", error))
+db.once("open", () => console.log("connect"))
+
+
 const postRoutes = require("./routes/postsRoute")
 app.use("/post", postRoutes)
 
