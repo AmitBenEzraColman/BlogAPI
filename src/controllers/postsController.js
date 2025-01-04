c487084b131101556d9bec8b354587edf487be8a
AmitBenEzraColman <amit922001@cs.colman.ac.il>
2025-01-05 00:37:37 +0200

    add model

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,27 +1,32 @@
-const getAllPosts = (req, res) => {
-console.log('posts')
-res.send('getAllPosts');
-}
+const postModel = require("../models/postsModel")
 
-const getPostBySender = (req, res) => {
-  res.send('getPostBySender');
+const getAllPosts = (req, res) => {
+  const sender = req.query.sender;
+  res.send('getPostBySender: ' + sender);
   }
 
 const getPostById = (req, res) => {
-  res.send('getPostById');
+  res.send('getPostById: '+ req.params.id);
   }
 
 const deletePostById = (req, res) => {
-  res.send('deletePostById');
+  res.send('deletePostById: '+ req.params.id);
   }
 
 const createPost = (req, res) => {
-  res.send('createPost');
+  const post = req.body;
+  postModel.create(post, (error, post) => {
+    if (error) {
+      res.status(400).send(error);
+    } else {
+      res.send(post);
+    }
+  });
+  res.send('createPost: ' + JSON.stringify(post));
   }
 
   module.exports = {
     getAllPosts,
-    getPostBySender,
     getPostById,
     deletePostById,
     createPost
--- src/routes/postsRoute.js
+++ src/routes/postsRoute.js
@@ -10,15 +10,12 @@ router.get('/:id', (req, res) => {
   postController.getPostById(req, res)
 });
 
-router.get('/:id', (req, res) => {
-  postController.getPostBySender(req, res)
-});
 
 router.post('/', (req, res) => {
   postController.createPost(req, res)
 });
 
-router.delete('/', (req, res) => {
+router.delete('/:id', (req, res) => {
   postController.deletePostById(req, res)
 });
 
