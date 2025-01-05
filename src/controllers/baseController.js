400da35f014b72421b6d03dd616daad5491cad1d
Yarin Hershko <yarin5805180@gmail.com>
2025-01-05 20:33:59 +0200

    Created base controller and update existing controllers

---
+++ src/controllers/baseController.js
@@ -0,0 +1,58 @@
+const { Model } = require("mongoose");
+
+class BaseController {
+  constructor(model) {
+    this.model = model;
+  }
+  async getAll(req, res) {
+    const filter = req.query.owner;
+    try {
+      if (filter) {
+        const item = await this.model.find({ sender: filter });
+        res.send(item);
+      } else {
+        const items = await this.model.find();
+        res.send(items);
+      }
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  }
+
+  async getById(req, res) {
+    const id = req.params.id;
+
+    try {
+      const item = await this.model.findById(id);
+      if (item != null) {
+        res.send(item);
+      } else {
+        res.status(404).send("not found");
+      }
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  }
+
+  async create(req, res) {
+    const body = req.body;
+    try {
+      const item = await this.model.create(body);
+      res.status(201).send(item);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  }
+
+  async deleteItem(req, res) {
+    const id = req.params.id;
+    try {
+      const rs = await this.model.findByIdAndDelete(id);
+      res.status(200).send("deleted");
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  }
+}
+
+module.exports = BaseController;
--- src/routes/postsRoute.js
+++ src/routes/postsRoute.js
@@ -1,22 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const postController = require("../controllers/postsController")
+const postController = require("../controllers/postsController");
 
-router.get('/', (req, res) => {
-  postController.getAllPosts(req, res)
+router.get("/", (req, res) => {
+  postController.getAll(req, res);
 });
 
-router.get('/:id', (req, res) => {
-  postController.getPostById(req, res)
+router.get("/:id", (req, res) => {
+  postController.getById(req, res);
 });
 
-
-router.post('/', (req, res) => {
-  postController.createPost(req, res)
+router.post("/", (req, res) => {
+  postController.create(req, res);
 });
 
-router.delete('/:id', (req, res) => {
-  postController.deletePostById(req, res)
+router.delete("/:id", (req, res) => {
+  postController.delete(req, res);
 });
 
 module.exports = router;
