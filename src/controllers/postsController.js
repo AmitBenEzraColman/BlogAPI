02fe27f5fc09c0b952256b2e5c459aa00a0e6ed0
Yarin Hershko <yarin5805180@gmail.com>
2025-01-05 20:34:31 +0200

    Created base controller and update existing controllers

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,61 +1,6 @@
-const postModel = require("../models/postsModel")
+const postModel = require("../models/postsModel");
+const BaseController = require("./baseController");
 
-const getAllPosts = async (req, res) => {
-  const sender = req.query.sender;
-  try {
-    let posts;
-    if(sender){
-      posts = await postModel.find({sender: sender});
-    } else {
-      posts = await postModel.find();
-    }
-    res.status(201).send(posts);
-   } 
-   catch(error){
-     res.status(400).send(error);
-   }
-  }
+const postsController = new BaseController(postModel);
 
-const getPostById = async (req, res) => {
-  const postId = req.params.id;
-  console.log('postId',postId)
-  try {
-    const posts = await postModel.findById(postId);
-    console.log('posts:' + posts)
-    res.status(201).send(posts);
-   } 
-   catch(error){
-     res.status(400).send(error);
-   }
-  }
-
-const deletePostById = async (req, res) => {
-    const id = req.params.id;
-  
-  try {
-    const posts = await postModel.findByIdAndDelete(id)
-    res.status(201).send(posts);
-   } 
-   catch(error){
-     res.status(400).send(error);
-   }
-  }
-
-const createPost = async (req, res) => {
-  const post = req.body;
-  try {
-   const newPost = await postModel.create(post)
-   res.status(201).send(newPost);
-  } 
-  catch(error){
-    res.status(400).send(error);
-  }
-  
-  }
-
-  module.exports = {
-    getAllPosts,
-    getPostById,
-    deletePostById,
-    createPost
-  }
+module.exports = postsController;
