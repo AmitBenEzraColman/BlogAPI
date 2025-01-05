bf6eaa562248b356b18b8ca8eb8eb70c74105ddc
Yarin Hershko <yarin5805180@gmail.com>
2025-01-05 20:35:20 +0200

    Change postId field to point on post Model

--- src/models/commentModel.js
+++ src/models/commentModel.js
@@ -11,7 +11,8 @@ const commentSchema = new Schema({
     required: true,
   },
   postId: {
-    type: String,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Comment",
     required: true,
   },
 });
