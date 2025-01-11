bab56f9820331d431b3f9c4ba4dbd613de9ffc55
AmitBenEzraColman <amit922001@cs.colman.ac.il>
2025-01-11 19:40:26 +0200

    add page json

--- src/tests/posts.test.ts
+++ src/tests/posts.test.ts
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import postModel from "../models/postsModel";
 import { Express } from "express";
 import userModel, { IUser } from "../models/userModel";
+import testPosts from "./testPosts.json";
 
 var app: Express;
 
@@ -15,7 +16,6 @@ const testUser: User = {
 }
 
 beforeAll(async () => {
-  console.log("beforeAll");
   app = await initBlogApiApp();
   await postModel.deleteMany();
 
@@ -35,7 +35,7 @@ afterAll((done) => {
 
 let postId = "";
 describe("Posts Tests", () => {
-  test("Posts test get all", async () => {
+  test("Empty Posts test get all", async () => {
     const response = await request(app).get("/posts");
     expect(response.statusCode).toBe(200);
     expect(response.body.length).toBe(0);
@@ -44,42 +44,39 @@ describe("Posts Tests", () => {
   test("Test Create Post", async () => {
     const response = await request(app).post("/posts")
       .set({ authorization: "JWT " + testUser.token })
-      .send({
-        title: "Test Post",
-        content: "Test Content",
-        owner: "TestOwner",
-      });
+      .send(testPosts[0]);
     expect(response.statusCode).toBe(201);
-    expect(response.body.title).toBe("Test Post");
-    expect(response.body.content).toBe("Test Content");
+    expect(response.body.title).toBe(testPosts[0].title);
+    expect(response.body.content).toBe(testPosts[0].content);
+    expect(response.body.sender).toBe(testPosts[0].sender);
     postId = response.body._id;
-  });
-
-  test("Test get post by owner", async () => {
-    const response = await request(app).get("/posts?owner=" + testUser._id);
-    expect(response.statusCode).toBe(200);
-    expect(response.body.length).toBe(1);
-    expect(response.body[0].title).toBe("Test Post");
-    expect(response.body[0].content).toBe("Test Content");
-  });
-
-  test("Test get post by id", async () => {
-    const response = await request(app).get("/posts/" + postId);
-    expect(response.statusCode).toBe(200);
-    expect(response.body.title).toBe("Test Post");
-    expect(response.body.content).toBe("Test Content");
   });
 
   test("Test Create Post 2", async () => {
     const response = await request(app).post("/posts")
       .set({ authorization: "JWT " + testUser.token })
-      .send({
-        title: "Test Post 2",
-        content: "Test Content 2",
-        owner: "TestOwner2",
-      });
+      .send(testPosts[1]);
     expect(response.statusCode).toBe(201);
+    expect(response.body.title).toBe(testPosts[1].title);
+    expect(response.body.content).toBe(testPosts[1].content);
+    expect(response.body.sender).toBe(testPosts[1].sender);
   });
+
+  test("Test get post by sender", async () => {
+    const response = await request(app).get("/posts?sender=" + testUser._id);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBe(1);
+    expect(response.body[0].title).toBe(testPosts[0].title);
+    expect(response.body[0].content).toBe(testPosts[0].content);
+  });
+
+  test("Test get post by id", async () => {
+    const response = await request(app).get("/posts/" + postId);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.title).toBe(testPosts[0].title);
+    expect(response.body.content).toBe(testPosts[0].content);
+  });
+
 
   test("Posts test get all 2", async () => {
     const response = await request(app).get("/posts");
@@ -99,7 +96,7 @@ describe("Posts Tests", () => {
     const response = await request(app).post("/posts")
       .set({ authorization: "JWT " + testUser.token })
       .send({
-        content: "Test Content 2",
+        content: "Test Content without title",
       });
     expect(response.statusCode).toBe(400);
   });
